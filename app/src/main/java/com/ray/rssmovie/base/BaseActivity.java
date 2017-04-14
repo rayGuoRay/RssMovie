81204cb3d1e6eaac1eaaf1ff797f60a96926f623
guolei <guolei@smartisan.com>
2017-04-14 19:23:52 +0800

    Feature: alter project and add library
    1) add library butter-knife
    2) alter folder of project
    
    Signed-off-by: gulei_ray

---
+++ app/src/main/java/com/ray/rssmovie/base/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.ray.rssmovie.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by guolei on 17-4-14.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+}
